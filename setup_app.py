da5bf1c333ed25a68407b677164323dd7f85c6d8
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-02-25 22:44:03 -0800

    implement authentication setup

--- setup_app.py
+++ setup_app.py
@@ -14,6 +14,11 @@ except:
 from pathlib import Path
 from shutil import copy
 from os import system
+from hashlib import md5
+import sqlite3
+
+def genHash(data:str) -> str:
+  return md5(data.encode('utf-8')).hexdigest()
 
 # copy config files & make data dir
 print('copy config files & make data dir\n')
@@ -33,9 +38,9 @@ with open('.env', 'r', encoding='utf-8') as file:
 env = dict( map(lambda i:i.split('='), envraw.splitlines()) )
 print('Do you want to change your application port?')
 print(f'Current application will be deployed on port:{env["APP_PORT"]}')
-port = input('Enter new port number if you want to change it, otherwise enter \"n\": ')
+port = input('Enter new port number if you want to change it, otherwise press \"Enter\": ')
 port = port.lower()
-if port != 'n':
+if not port.isnumeric():
   try:
     port = int(port)
     env['APP_PORT'] = port
@@ -45,6 +50,34 @@ if port != 'n':
     print(f'set application port to: {port}')
   except Exception as err:
     print(f'Invalid port number or failed to update port number, exception: {err}')
+
+# ask for creating authentication
+print('update authentication info')
+opt = input('Do you want to setup authentication? (Y/n): ')
+if opt.lower() == 'y' or len(opt) == 0:
+  # setup auth_db
+  with open('ShareTitle/src/main/resources/sql/init_auth_db.sql', 'r', encoding='utf-8') as file:
+    sql = file.read()
+  dbpath = dest/'sharetitle_auth.sqlite'
+  conn = sqlite3.connect(dbpath)
+  cur = conn.cursor()
+  cur.executescript(sql)
+  conn.commit()
+  
+  # ask for username & password
+  username = input('Enter username: ')
+  password = input('Enter password: ')
+  auth_hash = genHash(username+password)
+  cur.execute(
+    'REPLACE INTO auth VALUES(?, ?);',
+    [username, auth_hash]
+  )
+  cur.execute(
+    'REPLACE INTO table_update_info VALUES("auth", STRFTIME("%s", "now"));'
+  )
+  conn.commit()
+  cur.close()
+  conn.close()
 
 # run docker-compose
 print('\nrun docker-compose\n')
